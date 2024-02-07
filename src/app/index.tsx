efed7f06c839a70ee3bee38cf08b2017f95ed008
Rogerio <rogeriopiatek@gmail.com>
2024-02-07 18:11:23 -0300

    feat: added initial Product component and product list

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,16 @@
-import { View, FlatList } from "react-native";
+import { View, FlatList, SectionList, Text } from "react-native";
 
-import { CATEGORIES } from "@/utils/data/products";
+import { CATEGORIES, MENU } from "@/utils/data/products";
+
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
 import { useState } from "react";
+import { Product } from "@/components/product";
 
 export default function Home() {
   const [category, setCategory] = useState(CATEGORIES[0]);
 
   function handleCategoryClick(selectedCategory: string) {
-    console.log(selectedCategory);
     setCategory(selectedCategory);
   }
 
@@ -31,6 +32,21 @@ export default function Home() {
         className="max-h-10 mt-5"
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
+
+      <SectionList
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => <Product data={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">
+            {title}
+          </Text>
+        )}
+        className="flex-1 p-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
       />
     </View>
   );
---
+++ src/components/product.tsx
@@ -0,0 +1,35 @@
+import {
+  Image,
+  ImageProps,
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from "react-native";
+
+interface ProductDataProps {
+  title: string;
+  description: string;
+  thumbnail: ImageProps;
+}
+
+interface ProductProps extends TouchableOpacityProps {
+  data: ProductDataProps;
+}
+
+export function Product({ data, ...rest }: ProductProps) {
+  return (
+    <TouchableOpacity className="w-full flex-row items-center pb-4" {...rest}>
+      <Image source={data.thumbnail} className="w-20 h-20 rounded-md" />
+
+      <View className="flex-1 ml-3">
+        <Text className="text-slate-100 font-subtitle text-base flex-1">
+          {data.title}
+        </Text>
+        <Text className="text-slate-400 text-xs leading-5 mt-0.5">
+          {data.description}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
