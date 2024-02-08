d49b402df2b1014d77664275bed0433ac033b6c0
Rogerio <rogeriopiatek@gmail.com>
2024-02-07 21:21:33 -0300

    feat: ensure user can remove Product from cart

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,11 +1,16 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView, Alert } from "react-native";
+import { Feather } from "@expo/vector-icons";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-import { useCartStore } from "@/stores/cart-store";
+import { ProductCartProps, useCartStore } from "@/stores/cart-store";
 
 import { formatCurrency } from "@/utils/functions/format-currency";
 
 import { Header } from "@/components/header";
+import { Button } from "@/components/button";
 import { Product } from "@/components/product";
+import { Input } from "@/components/input";
+import { LinkButton } from "@/components/link-button";
 
 export default function Cart() {
   const cartStore = useCartStore();
@@ -17,26 +22,66 @@ export default function Cart() {
     )
   );
 
+  function handleProductRemove(product: ProductCartProps) {
+    Alert.alert("Remover", `Deseja remover ${product.title} do carrinho?`, [
+      {
+        text: "Cancelar",
+      },
+      {
+        text: "Remover",
+        onPress: () => cartStore.remove(product.id),
+      },
+    ]);
+  }
+
   return (
     <View className="flex-1 pt-8">
       <Header title="Seu carrinho" />
 
-      {cartStore.products.length > 0 ? (
-        <View className="p-5 flex-1">
-          {cartStore.products.map((product) => (
-            <Product key={product.id} data={product} />
-          ))}
-        </View>
-      ) : (
-        <Text className="font-body text-slate-400 text-center my-8">
-          Seu carrinho está vazio!
-        </Text>
-      )}
+      <KeyboardAwareScrollView
+        showsVerticalScrollIndicator={false}
+        extraHeight={100}
+      >
+        <ScrollView>
+          <View className="p-5 flex-1">
+            {cartStore.products.length > 0 ? (
+              <View className="border-b border-slate-700">
+                {cartStore.products.map((product) => (
+                  <Product
+                    key={product.id}
+                    data={product}
+                    onPress={() => handleProductRemove(product)}
+                  />
+                ))}
+              </View>
+            ) : (
+              <Text className="font-body text-slate-400 text-center my-8">
+                Seu carrinho está vazio!
+              </Text>
+            )}
 
-      <View className="flex-row gap-2 items-center mt-5 mb-4">
-        <Text className="text-white text-xl font-subtitle">Total:</Text>
+            <View className="flex-row gap-2 items-center mt-5 mb-4">
+              <Text className="text-white text-xl font-subtitle">Total:</Text>
 
-        <Text className="text-lime-400 text-2xl font-heading">{total}</Text>
+              <Text className="text-lime-400 text-2xl font-heading">
+                {total}
+              </Text>
+            </View>
+
+            <Input placeholder="Informe o endereço de entrega com rua, número, bairro, CEP e complemento..." />
+          </View>
+        </ScrollView>
+      </KeyboardAwareScrollView>
+
+      <View className="p-5 gap-5">
+        <Button>
+          <Button.Text>Enviar pedido</Button.Text>
+          <Button.Icon>
+            <Feather name="arrow-right-circle" size-20 />
+          </Button.Icon>
+        </Button>
+
+        <LinkButton title="Voltar ao cardápio" href="/" />
       </View>
     </View>
   );
