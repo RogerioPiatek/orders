34b224c3ef16a3ca7f004d9c208b9e1a46160954
Rogerio <rogeriopiatek@gmail.com>
2024-02-07 21:27:41 -0300

    feat: ensure Orders are persistent on user's phone

--- src/stores/cart-store.ts
+++ src/stores/cart-store.ts
@@ -1,5 +1,8 @@
-import { ProductProps } from "@/utils/data/products";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { ProductProps } from "@/utils/data/products";
 import * as cartInMemory from "./helpers/cart-in-memory";
 
 export interface ProductCartProps extends ProductProps {
@@ -12,16 +15,24 @@ interface StateProps {
   remove: (productId: string) => void;
 }
 
-export const useCartStore = create<StateProps>((set) => ({
-  products: [],
-  add: (product: ProductProps) =>
-    set((state) => ({
-      products: cartInMemory.add(state.products, product),
-    })),
+export const useCartStore = create(
+  persist<StateProps>(
+    (set) => ({
+      products: [],
+      add: (product: ProductProps) =>
+        set((state) => ({
+          products: cartInMemory.add(state.products, product),
+        })),
 
-  remove: (productId: string) => {
-    set((state) => ({
-      products: cartInMemory.remove(state.products, productId),
-    }));
-  },
-}));
+      remove: (productId: string) => {
+        set((state) => ({
+          products: cartInMemory.remove(state.products, productId),
+        }));
+      },
+    }),
+    {
+      name: "orders:cart",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
