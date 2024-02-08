157caf55beb5d19b48955ade4a47a1002563d606
Rogerio <rogeriopiatek@gmail.com>
2024-02-07 22:13:38 -0300

    fix: ensure product's description is properly displayed on smaller devices

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -1,4 +1,4 @@
-import { Image, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { Redirect } from "expo-router";
 import { Feather } from "@expo/vector-icons";
@@ -38,27 +38,31 @@ export default function Product() {
         resizeMode="cover"
       />
 
-      <View className="p-5 mt-8 flex-1">
-        <Text className="text-white text-xl font-heading">{product.title}</Text>
-
-        <Text className="text-lime-400 text-2xl font-heading my-2">
-          {formatCurrency(product.price)}
-        </Text>
-
-        <Text className="text-slate-400 font-body text-base leading-6 mb-6">
-          {product.description}
-        </Text>
-
-        {product.ingredients.map((ingredient) => (
-          <Text
-            key={ingredient}
-            className="text-slate-400 font-body text-base leading-6"
-          >
-            {"\u2022"}
-            {ingredient}
+      <ScrollView>
+        <View className="p-5 mt-8 flex-1">
+          <Text className="text-white text-xl font-heading">
+            {product.title}
           </Text>
-        ))}
-      </View>
+
+          <Text className="text-lime-400 text-2xl font-heading my-2">
+            {formatCurrency(product.price)}
+          </Text>
+
+          <Text className="text-slate-400 font-body text-base leading-6 mb-6">
+            {product.description}
+          </Text>
+
+          {product.ingredients.map((ingredient) => (
+            <Text
+              key={ingredient}
+              className="text-slate-400 font-body text-base leading-6"
+            >
+              {"\u2022"}
+              {ingredient}
+            </Text>
+          ))}
+        </View>
+      </ScrollView>
 
       <View className="p-5 pb-8 gap-5">
         <Button onPress={handleAddToCart}>
