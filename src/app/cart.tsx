d8596bea46c1ef3da08cba174c6826f518ea2cab
Rogerio <rogeriopiatek@gmail.com>
2024-02-07 22:10:42 -0300

    feat: ensure order is sent to Whatsapp when completed

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,4 +1,6 @@
-import { Text, View, ScrollView, Alert } from "react-native";
+import { useState } from "react";
+import { useNavigation } from "expo-router";
+import { Text, View, ScrollView, Alert, Linking } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
@@ -12,8 +14,13 @@ import { Product } from "@/components/product";
 import { Input } from "@/components/input";
 import { LinkButton } from "@/components/link-button";
 
+//Provide your number so Whatsapp sends to you
+const PHONE_NUMBER = "";
+
 export default function Cart() {
+  const [address, setAddress] = useState("");
   const cartStore = useCartStore();
+  const navigation = useNavigation();
 
   const total = formatCurrency(
     cartStore.products.reduce(
@@ -32,6 +39,32 @@ export default function Cart() {
         onPress: () => cartStore.remove(product.id),
       },
     ]);
+  }
+
+  function handleOrder() {
+    if (address.trim().length === 0) {
+      return Alert.alert("Pedido", "Informe os dados da entrega.");
+    }
+
+    const products = cartStore.products
+      .map((product) => `\n ${product.quantity} ${product.title}`)
+      .join("");
+
+    const message = `
+    🍔 NOVO PEDIDO
+    \n Entregar em: ${address}
+
+    ${products}
+
+    \n Valor total: ${total} 
+    `;
+
+    Linking.openURL(
+      `http://api.whatsapp.com/send?phone=${PHONE_NUMBER}&text=${message}`
+    );
+
+    cartStore.clear();
+    navigation.goBack();
   }
 
   return (
@@ -68,13 +101,16 @@ export default function Cart() {
               </Text>
             </View>
 
-            <Input placeholder="Informe o endereço de entrega com rua, número, bairro, CEP e complemento..." />
+            <Input
+              placeholder="Informe o endereço de entrega com rua, número, bairro, CEP e complemento..."
+              onChangeText={setAddress}
+            />
           </View>
         </ScrollView>
       </KeyboardAwareScrollView>
 
       <View className="p-5 gap-5">
-        <Button>
+        <Button onPress={handleOrder}>
           <Button.Text>Enviar pedido</Button.Text>
           <Button.Icon>
             <Feather name="arrow-right-circle" size-20 />
